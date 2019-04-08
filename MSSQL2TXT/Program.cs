72eb501c5e6b4e9139dd8e81e6bcd404d553c1e5
qqj1228 <qqj1228@gmail.com>
2019-04-08 14:32:59 +0800

    first commit

---
+++ MSSQL2TXT/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MSSQL2TXT {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
